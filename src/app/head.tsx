cf868ee20fa57fdd779724fac1cf01535756e5e5
Oren Sharizad <oren.sharizad12@gmail.com>
2023-03-23 17:46:43 +0200

    first commit

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,10 +1,20 @@
+// export default function head() {
+//     return (
+//         <>
+//             <title>Airbnb</title>
+//             <meta content="width=device-width, initial-scale=1" name="viewport" />
+//             <link rel="icon" href="https://airbnb-in-react.netlify.app/static/media/mini-logo.1b5ae477397a3f99ffec204d164afd75.svg" />
+
+//         </>
+//     )
+// }
+
 export default function head() {
     return (
         <>
-            <title>Airbnb</title>
+            <title>airbnb</title>
             <meta content="width=device-width, initial-scale=1" name="viewport" />
             <link rel="icon" href="https://airbnb-in-react.netlify.app/static/media/mini-logo.1b5ae477397a3f99ffec204d164afd75.svg" />
-
         </>
     )
 }
